6bd037eb006e8108bd10af3582ccdf8eeb8afba9
sathvik-gadipelli <company.rosh07@gmail.com>
2026-03-21 23:44:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Razorpay from "razorpay";
 import cors from "cors";
+import crypto from "crypto";
 
 const app = express();
 
@@ -8,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const razorpay = new Razorpay({
-  key_id: "rzp_test_STv4z5jBSS1Pal",
-  key_secret: "HZXeDSZSBUvVnC4Az4WEoser"
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 app.get("/", (req, res) => {
@@ -18,16 +19,23 @@ app.get("/", (req, res) => {
 
 app.post("/create-order", async (req, res) => {
   try {
+    const amount = Number(req.body.amount);
+
+    if (!amount || amount < 100) {
+      return res.status(400).send("Invalid amount");
+    }
+
     const options = {
-      amount: req.body.amount,
+      amount: amount,
       currency: "INR"
     };
 
     const order = await razorpay.orders.create(options);
+
     res.json(order);
 
   } catch (err) {
-    console.error(err);
+    console.error("RAZORPAY ERROR:", err);
     res.status(500).send("Error creating order");
   }
 });
@@ -36,4 +44,31 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
+});
+
+app.post("/verify-payment", (req, res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature
+    } = req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", "HZXeDSZSBUvVnC4Az4WEoser") // 🔥 YOUR SECRET
+      .update(body.toString())
+      .digest("hex");
+
+    if (expectedSignature === razorpay_signature) {
+      res.json({ success: true });
+    } else {
+      res.status(400).json({ success: false });
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false });
+  }
 });
